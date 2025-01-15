bc35aadfde0de4a758e8c8e52a13895a25bbf9be
ahig0ne <sebaselnub@gmail.com>
2025-01-15 12:36:53 -0500

    :zap: feat : update

--- js/frontpage.js
+++ js/frontpage.js
@@ -7,13 +7,16 @@ export const renderizarPeliculas = peliculas => {
       <img class="movie__img" src="${pelicula.img}" alt="">
       <style>
       .movie__img{
+        border-radius: 2px;
         width: 250px;
-        height: 250px;
+        height: 310px;
       </style>
       <div class="movie__title"><h1 class="title">${pelicula.titulo}</h1></div>
 
       <div class="movie__info">${pelicula.resumen}</div>
-      <div class="movie__vm">
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    Launch demo modal
+    </button>
     `; //Se agrega al HTML las clases de la tarjeta
 
     movieList.appendChild(listItem); // Se envia la informacion al html
